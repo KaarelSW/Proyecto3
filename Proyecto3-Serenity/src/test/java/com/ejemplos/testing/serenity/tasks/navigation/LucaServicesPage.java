48cde8ac486fa8d1799417733e90cf72f84d7013
Vieris <alex12293@gmail.com>
2022-04-04 17:04:11 +0200

    cambios base serenity

--- Proyecto3-Serenity/src/test/java/com/ejemplos/testing/serenity/tasks/navigation/LucaServicesPage.java
+++ Proyecto3-Serenity/src/test/java/com/ejemplos/testing/serenity/tasks/navigation/LucaServicesPage.java
@@ -3,6 +3,6 @@ package com.ejemplos.testing.serenity.tasks.navigation;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("https://wikipedia.org")
-public class WikipediaHomePage extends PageObject {
+@DefaultUrl("https://lucaticenterprise.herokuapp.com/service")
+public class LucaServicesPage extends PageObject {
 }
