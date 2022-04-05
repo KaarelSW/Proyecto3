439fab51d2e156cb9bf3d7b5732206c58cef76b5
MiguelAngelFM <100855287+MiguelAngelFM@users.noreply.github.com>
2022-04-05 15:19:11 +0100

    navbar

---
+++ Proyecto3-Serenity/src/test/java/com/ejemplos/testing/serenity/tasks/navbar/Navbar.java
@@ -0,0 +1,10 @@
+package com.ejemplos.testing.serenity.tasks.navbar;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+class NavBar {
+
+	static Target SEARCH_FIELD = Target.the("search field").locatedBy("#searchInput");
+	
+
+}
