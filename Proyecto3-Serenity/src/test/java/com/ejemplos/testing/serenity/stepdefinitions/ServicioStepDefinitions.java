3a8aadbf069850eb94e3b9d322c9e672aac21296
xn0 <chanomm75@gmail.com>
2022-04-05 14:53:19 +0200

    Test1 correcciones

--- Proyecto3-Serenity/src/test/java/com/ejemplos/testing/serenity/stepdefinitions/ServicioStepDefinitions.java
+++ Proyecto3-Serenity/src/test/java/com/ejemplos/testing/serenity/stepdefinitions/ServicioStepDefinitions.java
@@ -1,27 +1,24 @@
 package com.ejemplos.testing.serenity.stepdefinitions;
 
-
-
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import com.ejemplos.testing.serenity.tasks.navigation.NavigateTo;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.core.Serenity;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.ensure.Ensure;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.JavascriptExecutor;
-import com.ejemplos.testing.serenity.tasks.navigation.NavigateTo;
-
 
 public class ServicioStepDefinitions{
 
 	
-    @Given("{actor} un usuario se encuentra en la web")
-    public void navigate_To_Luca_Service_Page(Actor actor) {
+    @Given("un usuario se encuentra en la web")
+    public void navigate_To_Luca_Service_Page() {
     
     }
 
-    @When("{actor} el usuario accede sección Servicios {string}")
+    @When("el usuario accede sección Servicios")
     public void acceder_Seccion_Servicios(Actor actor) {
 		
         actor.attemptsTo(
@@ -31,7 +28,7 @@ public class ServicioStepDefinitions{
     }
 
     @Then("se carga la página Servicios y se muestran los contenidos correctamente")
-    public void deberia_cargar_y_mostrar_contenido_correctamente(Actor actor) {
+    public void deberia_cargar_y_mostrar_contenido_correctamente() {
 
     	WebDriver driver = Serenity.getDriver();
     	JavascriptExecutor j = (JavascriptExecutor) driver;
--- Proyecto3-Serenity/src/test/resources/features/Servicios/Servicio.feature
+++ Proyecto3-Serenity/src/test/resources/features/Servicios/Servicio.feature
@@ -1,7 +1,10 @@
 Feature: un usuario accede a la pagina Servicios correctamente
 
+Scenario: comprobar que la pagina servicios se carga correctamente
+
   Given un usuario se encuentra en la web
 
   When el usuario accede sección Servicios
 
   Then se carga la página Servicios y se muestran los contenidos correctamente
+  
