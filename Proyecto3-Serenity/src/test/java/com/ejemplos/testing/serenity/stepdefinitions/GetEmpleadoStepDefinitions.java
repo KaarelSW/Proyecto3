f6359bc1f3353025b3d0aa350c380f99b205a631
MiguelAngelFM <100855287+MiguelAngelFM@users.noreply.github.com>
2022-04-07 11:15:54 +0100

    Fin getEmpleado

--- Proyecto3-Serenity/src/test/java/com/ejemplos/testing/serenity/stepdefinitions/GetEmpleadoStepDefinitions.java
+++ Proyecto3-Serenity/src/test/java/com/ejemplos/testing/serenity/stepdefinitions/GetEmpleadoStepDefinitions.java
@@ -56,17 +56,17 @@ public class GetEmpleadoStepDefinitions{
     
 //Feature: Consultar un empleado. Escenario 1
 	
-    @Given("^un administrador está logueado$")
+    @Given("un administrador está logueado")
     public void un_administrador_logueado() throws Exception {
         RestAssured.baseURI  = url;
         request = RestAssured.given();
     }
     
     Response response;
-    @When("pide los datos de un empleado específico$")
-    public void realiza_una_peticion_a_la_API() throws Exception {
-    	int idEmpleado = 1;
-    	response = request.get("/empleados/"+idEmpleado);
+    @When("pide los datos de un empleado específico con id {int}")
+    public void realiza_una_peticion_a_la_API(int id) throws Exception {
+    	//int idEmpleado = 42;
+    	response = request.get("/empleados/"+id);
     }
 
     @Then("^se obtiene un JSON con la información del empleado$")
@@ -131,6 +131,24 @@ public class GetEmpleadoStepDefinitions{
     
 //Feature: Consultar un empleado. Escenario 8     
     
+    @Then("^se devuelve un empleado cuya propriedad teléfono tiene un formato correcto$")
+    public void devuelve_un_objeto_cuya_propiedad_telefono_tiene_formato_correcto() throws Exception {
+    	JSONObject json = new JSONObject(response.body().asString());
+    	String telefono = json.getJSONObject("data").get("telefono").toString();
+    	Assert.assertTrue(Pattern.matches("^[+]*[0-9]{2} [(]{1}[0-9]{3}[)] {1}[0-9]{3}[-]{1}[0-9]{4}$", telefono));
+    }
+    
+//Feature: Consultar un empleado. Escenario 9    
+    
+    @Then("^se devuelve un empleado cuya propiedad género está compuesta por caracteres alfabéticos$")
+    public void devuelve_un_objeto_cuyo_genero_solo_tiene_letras() throws Exception {
+    	JSONObject json = new JSONObject(response.body().asString());
+    	String genero = json.getJSONObject("data").get("genero").toString();
+    	Assert.assertTrue(Pattern.matches("^[a-zA-ZáéíóúÁÉÍÓÚ]*$", genero));
+    }
+  
+//Feature: Consultar un empleado. Escenario 10    
+    
     @Then("^se devuelve un empleado cuya propiedad registered contiene una fecha anterior o igual a la actual$")
     public void devuelve_un_objeto_cuya_propiedad_registered_contiene_fecha_no_futura() throws Exception {
     	JSONObject json = new JSONObject(response.body().asString());
@@ -142,5 +160,39 @@ public class GetEmpleadoStepDefinitions{
     	System.out.println(formatter.toString());
     	System.out.println(fechaRegistro.toString());
     	Assert.assertFalse(fechaRegistro.isAfter(fechaActual));
+    }
+    
+//Feature: Consultar un empleado. Escenario 11
+    
+    @Then("^se devuelve un empleado cuya propiedad geolocalización es un array de dos elementos$")
+    public void devuelve_un_objeto_cuya_propiedad_geolocalizacion_es_un_array_de_dos_elementos() throws Exception {
+    	JSONObject json = new JSONObject(response.body().asString());
+    	Assert.assertTrue(json.getJSONObject("data").getJSONArray("geolocalizacion").getJSONObject(0).length() == 2);
+    }
+    
+//Feature: Consultar un empleado. Escenario 12
+    
+    @Then("^se devuelve un empleado cuya propiedad latitud es un número con decimales entre -90 y 90$")
+    public void devuelve_un_objeto_cuya_propiedad_latitud_es_correcta() throws Exception {
+    	JSONObject json = new JSONObject(response.body().asString());
+    	double latitud = Double.parseDouble(json.getJSONObject("data").getJSONArray("geolocalizacion").getJSONObject(0).get("latitud").toString());
+    	Assert.assertTrue(latitud < 90 && latitud > -90);
+    }
+    
+//Feature: Consultar un empleado. Escenario 13
+    
+    @Then("^se devuelve un empleado cuya propiedad longitud es un número con decimales entre -180 y 180$")
+    public void devuelve_un_objeto_cuya_propiedad_longitud_es_correcta() throws Exception {
+    	JSONObject json = new JSONObject(response.body().asString());
+    	double longitud = Double.parseDouble(json.getJSONObject("data").getJSONArray("geolocalizacion").getJSONObject(0).get("longitud").toString());
+    	Assert.assertTrue(longitud < 180 && longitud > -180);
+    }
+    
+//Feature: Consultar un empleado. Escenario 14
+    
+    @Then("^se devuelve un empleado cuya propiedad login es un array de dos elementos$")
+    public void devuelve_un_objeto_cuya_propiedad_login_tiene_dos_elementos() throws Exception {
+    	JSONObject json = new JSONObject(response.body().asString());
+    	Assert.assertTrue(json.getJSONObject("data").getJSONArray("login").getJSONObject(0).length() == 2);
     }
 }
