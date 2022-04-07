13e91255855aa64a5d28df89c64117e1da66968e
KaarelSW <carlosmarincruz99@gmail.com>
2022-04-07 16:29:37 +0200

    BAse para trabajar con la página equipo

---
+++ Proyecto3-Serenity/src/test/java/com/ejemplos/testing/serenity/stepdefinitions/EquipoStepDefinitions.java
@@ -0,0 +1,46 @@
+package com.ejemplos.testing.serenity.stepdefinitions;
+import java.util.List;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.core.Serenity;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.actions.Scroll;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.Hit;
+import net.serenitybdd.screenplay.actions.JavaScriptClick;
+import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.actors.OnStage;
+
+import net.serenitybdd.screenplay.ensure.Ensure;
+import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
+import net.serenitybdd.screenplay.questions.WebElementQuestion;
+import net.serenitybdd.screenplay.targets.Target;
+import net.serenitybdd.screenplay.ui.Button;
+import net.serenitybdd.screenplay.waits.Wait;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.ejemplos.testing.serenity.tasks.footer.Footer;
+import com.ejemplos.testing.serenity.tasks.navigation.NavigateTo;
+
+
+
+public class EquipoStepDefinitions{	
+	
+	WebDriver driver = Serenity.getDriver();
+
+	
+
+}
---
+++ Proyecto3-Serenity/src/test/resources/features/equipo/equipo.feature
@@ -0,0 +1,2 @@
+@REQ_P3Q-12
+Feature: Como usuario quiero poder acceder a la página Equipo de la web para poder visualizar los miembros del equipo de desarrollo
