5f0b05705fad1721a3236367a9545cd8eaafa944
carlosjesuspm <permarcar96@gmail.com>
2022-04-07 16:03:20 +0200

    Ver Contacto passed

--- Proyecto3-Serenity/src/test/java/com/ejemplos/testing/serenity/stepdefinitions/ContactoStepDefinitions.java
+++ Proyecto3-Serenity/src/test/java/com/ejemplos/testing/serenity/stepdefinitions/ContactoStepDefinitions.java
@@ -24,14 +24,48 @@ import net.serenitybdd.screenplay.waits.Wait;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.ejemplos.testing.serenity.tasks.navbar.NavBar;
+import com.ejemplos.testing.serenity.tasks.navigation.NavigateTo;
 
 
 
 public class ContactoStepDefinitions{	
 
-    //Tonto quien lo lea ;)
+	@Given("un {actor} en la página Contacto")
+	public void un_usuario_en_la_pagina_contacto(Actor actor) {
+	actor.wasAbleTo(
+			NavigateTo.theLucaContactPage()
+			);
+	}
+	
+	@Given("un {actor} en la página Home")
+    public void is_in_home_page(Actor actor) {
+        actor.wasAbleTo(
+        		NavigateTo.theLucaHomePage()
+        );
+    }
+	
+	
+	@When ("ese {actor} hace click en ir a Contacto")
+	public void hace_click_pagina_Contacto(Actor actor) {
+		
+		actor.attemptsTo(JavaScriptClick.on(NavBar.CONTACTO_BUTTON));
+	}
+
+	
+	@Then("la página Contacto carga correctamente")
+	public void pagina_Contacto_Carga_Correctamente(){
+		
+		WebDriver driver = Serenity.getDriver();
+    	JavascriptExecutor j = (JavascriptExecutor) driver;
+    	Ensure.that(j.executeScript("return document.readyState").toString()).contains("complete");
+	}
+	
     
 }
--- Proyecto3-Serenity/src/test/resources/features/contacto/contacto.feature
+++ Proyecto3-Serenity/src/test/resources/features/contacto/contacto.feature
@@ -1,6 +1,16 @@
+@REQ_P3Q-11
 Feature: Como usuario quiero poder acceder al formulario en la página Contacto para enviar una solicitud a la empresa
 
 Scenario: Un usuario en la página de contacto visualiza los términos de uso
   Given un usuario en la página Contacto
   When clickea en los términos de uso
   Then te redirige a la correspondiente página
+ 
+
+@TEST_P3Q-103 @TESTSET_P3Q-36 @Web
+Scenario: Un usuario entra a la página Contacto
+  Given un usuario en la página Home
+  When ese usuario hace click en ir a Contacto
+  Then la página Contacto carga correctamente
+  
+
